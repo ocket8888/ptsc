36ae0ec7dc45b4773ca84c0f5c5c86ae8b670f67
ocket8888 <ocket8888@gmail.com>
2017-01-29 13:37:48 -0700

    First push, currently ignoring pycache

---
+++ compiler.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os
+from utilities import normalizePath
+
+def findConfigFile(searchPath):
+	while True:
+		if os.path.isfile(searchPath+"/tsconfig.json"):
+			return searchPath+"/tsconfig.json"
+		newPath = os.path.dirname(searchPath)
+		if newPath == searchPath:
+			return None
+		searchPath = newPath
+
+def resolveTripleslashReference(moduleName, containingFile):
+	basepath = os.path.dirname(containingFile)
+	return moduleName if moduleName[0] == "/" else basepath + "/" + moduleName
+
+def computeCommonSourceDirectoryOfFilenames(fileNames, currentDirectory):
+	commonPathComponents = list()
+	for sourceFile in fileNames:
+		sourcePath = currentDirectory+"/"+sourceFile if sourceFile[0] != "/" else sourceFile
+		sourcePathComponents = normalizePath(sourcePath)
+		
+		if len(commonPathComponents) == 0:
+			commonPathComponents = sourcePathComponents
+			continue
+
+		for i in range(0, min((len(commonPathComponents), len(sourcePathComponents)))):
+			if commonPathComponents[i] != sourcePathComponents[i]:
+				if i == 0:
+					return ""
+
+			commonPathComponents = commonPathComponents[:i]
+			break
+
+		if len(sourcePathComponents) < len(commonPathComponents):
+			commonPathComponents = commonPathComponents[:i]
+
+	if len(commonPathComponents) == 0:
+		return currentDirectory
+
+	return "/"+"/".join(commonPathComponents)
