842349ff17ec6f5c9f669fbad6f552d5940cdf65
ocket8888 <ocket8888@apache.org>
2020-07-06 18:10:53 -0600

    Fixed code folding that was driving me insane

--- ptsc/lexer.py
+++ ptsc/lexer.py
@@ -60,21 +60,17 @@ class Lexer():
 
 		>>> ipt = '''let five = 5;\
 		let ten = 10;\
-\
 		let add = function(x, y) {\
 			x + y;\
 		};\
-\
 		let result = add(five, ten);\
 		!-/*5;\
 		5 < 10 > 5;\
-\
 		if (5 < 10) {\
 			return true;\
 		} else {\
 			return false;\
 		}\
-\
 		10 == 10;\
 		10 != 9;\
 		"foobar"\
